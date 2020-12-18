5a577e3cf889b5f3b3ba3940c4b3f9aeb97209a9
Luis Zapata <jhonayquer@gmail.com>
2020-12-17 22:03:21 -0500

    Clase Paciente

--- src/Usuarios/Paciente.java
+++ src/Usuarios/Paciente.java
@@ -1,8 +1,39 @@
 package Usuarios;
 
-public class Paciente extends Usuarios {
+import java.util.Date;
 
-    public Paciente() {
-        super();
+public class Paciente extends Usuarios {
+    private boolean estadoPaciente;
+    private Date fechaCitaPaciente;
+    
+    public Paciente(String nombrePaciente, String dniPaciente, String contraseñaPaciente, boolean estadoPaciente, Date fechaCitaPaciente) {
+        super(nombrePaciente, dniPaciente, contraseñaPaciente);
+        this.estadoPaciente = estadoPaciente;
+        this.fechaCitaPaciente = fechaCitaPaciente;
     }
+    
+    public String getNombrePaciente() {
+        return super.getNombre();
+    }
+
+    public String getDniPaciente() {
+        return super.getDNI();
+    }
+
+    public boolean getEstadoPaciente() {
+        return estadoPaciente;
+    }
+
+    public void setEstadoPaciente(boolean estadoPaciente) {
+        this.estadoPaciente = estadoPaciente;
+    }
+    
+    public Date getFechaCitaPaciente() {
+        return fechaCitaPaciente;
+    }
+
+    public void setFechaCitaPaciente(Date fechaCitaPaciente) {
+        this.fechaCitaPaciente = fechaCitaPaciente;
+    }
+    
 }
