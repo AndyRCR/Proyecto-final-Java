ec7df728f074182df98040d0ffd7ec3681d7cdcb
Luis Zapata <jhonayquer@gmail.com>
2020-12-17 22:17:23 -0500

    Clase Hospital

---
+++ src/Usuarios/Hospital.java
@@ -0,0 +1,19 @@
+
+package Usuarios;
+
+public class Hospital {
+    private String nombreHospital;
+    private int aforo;
+
+    public String getNombreHospital() {
+        return nombreHospital;
+    }
+
+    public void setNombreHospital(String nombreHospital) {
+        this.nombreHospital = nombreHospital;
+    }
+
+    public int getAforo() {
+        return aforo;
+    }
+}
