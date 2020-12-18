bc3f419b8a32ddd545fa1414ee0d76b14986e706
Luis Zapata <jhonayquer@gmail.com>
2020-12-17 22:04:30 -0500

    Clase Medico

--- src/Usuarios/Medico.java
+++ src/Usuarios/Medico.java
@@ -1,8 +1,15 @@
 package Usuarios;
 
 public class Medico extends Usuarios {
-
-    public Medico() {
-        super();
+    private int codigoMedico;
+    
+    public Medico(String nombreMedico, String dniMedico, String contraseñaMedico, int codigoMedico) {
+        super(nombreMedico, dniMedico, contraseñaMedico);
+        this.codigoMedico = codigoMedico;
     }
+    
+    public String getNobreMedico(){
+        return super.getNombre();
+    }
+    
 }
