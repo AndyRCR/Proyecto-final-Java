0758bc1ab4f3539ea6d8344ae9ecc39b62576306
AndyRCR <andycr321@gmail.com>
2021-01-14 00:27:19 -0500

    Correccion conexion

--- src/BaseDeDatos/TestConexion.java
+++ src/BaseDeDatos/TestConexion.java
@@ -7,29 +7,6 @@ import java.util.logging.Logger;
 
 public class TestConexion {
     
-    private static Connection conn;
-    private static final String driver = "com.mysql.jdbc.Driver";
-    private static final String user = "root";
-    private static final String psw = "1234";
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/bdhospital";
-
-    public static Connection abrir(){
-        try {
-            //registrar drivers para mysql
-            Class.forName("com.mysql.jdbc.Driver");
-            //abrir conexion
-            conn=DriverManager.getConnection(url, user, psw);
-        } catch (Exception ex) {
-            Logger.getLogger(TestConexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return conn;
-    }
-    
-    public Connection getConnection() {
-        return conn;
-    }
-    
-    /*
     //variables
     private static String url="jdbc:mysql://localhost:33061/bdhospital";
     private static String user="root";
@@ -45,5 +22,5 @@ public class TestConexion {
             Logger.getLogger(TestConexion.class.getName()).log(Level.SEVERE, null, ex);
         }
         return cn;
-    }*/
+    }
 }
